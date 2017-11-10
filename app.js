03517551bb2c48b17d29298e0230e6e27dd449c4
allenhj <allendotjohnson@gmail.com>
2017-11-10 17:14:45 -0500

    Back on track

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ nunjucks.configure('views', {noCache: true});
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 
-app.use(express.static('./public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res, next) {
-  res.send('./views/index.html');
+  res.render('index');
 });
 
 app.listen(1337, function() {
