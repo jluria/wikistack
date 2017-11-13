6a4d737ce2831afbcc14f79010f0d927174ba3e6
Josh Luria <josh.luria@gmail.com>
2017-11-13 12:26:45 -0500

    Add RESTful routes for User model

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,26 @@ var router = express.Router();
 module.exports = router;
 
 router.get('/', function(req, res, next) {
+  db.User.findAll({})
+  .then(function(){
+    res.redirect('/');
+  })
+  .catch(function(error){
+    console.error(error)});
+});
 
+router.get('/:user', function(req, res, next) {
+  res.redirect('/');
+});
+
+router.post('/', function(req, res, next) {
+  res.redirect('/');
+});
+
+router.put('/:user', function(req, res, next) {
+  res.redirect('/');
+});
+
+router.delete('/:user', function(req, res, next) {
   res.redirect('/');
 });
